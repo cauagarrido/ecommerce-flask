a146a70f7a2198e1c1dda53a185d79d3de30bfde
cauagarrido <cauagarrido@gmail.com>
2025-05-28 17:09:07 -0300

    banco de dados criado

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ db = SQLAlchemy(app)
 
 #modelagem banco
 
-
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
 #rotas
 @app.route('/teste')
