8e04e72f4c330e863090cd18a4fe83ce4b36e73d
cauagarrido <cauagarrido@gmail.com>
2025-05-28 16:52:46 -0300

    teste de rotas e criação script do banco

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+#modelagem banco
+
+
 
 #rotas
-@app.route('/')
+@app.route('/teste')
 
 def hello_world():
     return 'hello world'
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
