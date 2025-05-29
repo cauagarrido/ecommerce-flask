fce234f76f8c8d7abf893d9e0fb270d0532a425e
Cauã Garrido <121697189+cauagarrido@users.noreply.github.com>
2025-05-28 23:17:11 -0300

    alteração e criação dos metodos delete e get

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db' 
+db = SQLAlchemy(app) 
 
-db = SQLAlchemy(app)
-
-#modelagem banco
-
+# Modelagem do banco de dados
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
@@ -15,15 +13,49 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=True)
 
 
+with app.app_context():
+    db.create_all() 
+
 @app.route('/api/products/add', methods=["POST"])
 def add_product():
     data = request.json
-    return data
-    
-    
-#rotas
-@app.route('/')
+    if not data or 'name' not in data or 'price' not in data:
+        return "Dados inválidos. 'name' e 'price' são obrigatórios.", 400
 
+    try:
+        price = float(data['price']) 
+    except ValueError:
+        return "O preço deve ser um número.", 400
+
+    product = Product(name=data['name'], price=price, description=data.get('description', ''))
+    db.session.add(product)
+    db.session.commit()
+    return "Produto cadastrado com sucesso!", 200
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return "Produto não encontrado.", 404
+
+    db.session.delete(product)
+    db.session.commit()
+    return "Produto excluído com sucesso!", 200
+
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonfiy({
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'description': product.description
+        })
+    return jsonify({"message:" "Produto não encontrado"}), 404
+   
+# Rotas
+@app.route('/')
 def hello_world():
     return 'hello world'
 
